4051f08fb6082c44e4e4d77021bc3b143ce5959a
ekkusa <dayesofficial@gmail.com>
2024-01-24 22:15:55 +0800

    Revert "tama-common: Keep building health HIDL interface lib in vendor"
    This reverts commit cb89a2b18cf8584701ee6fc6d2a44ec7e8ff6e6c.

--- common.mk
+++ common.mk
@@ -279,7 +279,6 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1.vendor \
     android.hardware.health-service.qti
 
 # HotwordEnrollement app permissions
